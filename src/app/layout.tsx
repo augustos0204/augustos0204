6afc16a9b7c4ddf7bf82c319ab5f63fdc85911ad
Augustos0204 <course.gabrielaug@gmail.com>
2024-09-23 07:07:17 -0300

    eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import LogoComponent from "@/components/logo";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
